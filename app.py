65944acf1629bbfea7d540e41d62ecfa2d2ff60d
老板1678 <410717263@qq.com>
2025-08-16 09:13:52 +0800

    代码更新

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from DrissionPage import ChromiumOptions, ChromiumPage
 from concurrent.futures import ThreadPoolExecutor
 from flask import Flask, request, jsonify
 import time
+import uuid, os, shutil
 
 app = Flask(__name__)
 
 
 
-def get_chrome_options():
+def get_chrome_options(temp_dir):
     co = ChromiumOptions()
 
     # ✅ 启用远程调试端口（必须）
@@ -27,37 +28,50 @@ def get_chrome_options():
 
     # ✅ 禁用扩展（提高稳定性）
     co.set_argument('--disable-extensions')
+
+    # 清除缓存
     co.incognito()
+
+    # 指定的浏览器缓存目录
+    co.user_data_dir = temp_dir
+
     # ✅ 禁用信息栏（防止“Chrome 正由自动化软件控制”提示）
     co.set_argument('--disable-infobars')
     co.set_argument('referer', 'https://www.douyin.com/search')
-    co.set_user_agent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36')
+    co.set_user_agent(
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36')
 
     return co
 
 
 def get_response(song_name):
     resp_lst = []
+    temp_dir = f'/tmp/chrome_profile_{uuid.uuid4()}'
+    os.makedirs(temp_dir, exist_ok=True)
+    co = get_chrome_options(temp_dir)
     url = f"https://www.douyin.com/search/{song_name}?aid=e8a1a21a-f91d-4f03-96ba-f14a7e8d37f7&type=general"
-    co = get_chrome_options()
     page = ChromiumPage(co)
-    # page.set.headers({'referer': 'https://www.douyin.com/search'})
     page.listen.start('general/search/single')
-    page.get(url)
-    page.wait(1)
-    for _ in range(3):
-        page.run_js('window.scrollBy(0, 1500)')
+    try:
+        page.get(url)
         page.wait(1)
-        res = page.listen.wait(timeout=0.5)
-        if not res or isinstance(res, bool):
-            continue
-        resp = res.response.body
-        if isinstance(resp, str):
-            continue
-        if resp in resp_lst:
-            continue
-        resp_lst.append(resp)
-    return resp_lst
+        for _ in range(3):
+            page.run_js('window.scrollBy(0, 1500)')
+            page.wait(1)
+            res = page.listen.wait(timeout=0.5)
+            if not res or isinstance(res, bool):
+                continue
+            resp = res.response.body
+            if isinstance(resp, str):
+                continue
+            if resp in resp_lst:
+                continue
+            resp_lst.append(resp)
+        return resp_lst
+    finally:
+        # ✅ 清理逻辑
+        page.quit()  # 关闭浏览器
+        shutil.rmtree(temp_dir, ignore_errors=True)
 
 
 def song_url(item):
@@ -109,13 +123,13 @@ def get_song_url(word):
     results = last(word)
     flat = [item for group in results for item in group]
     return jsonify({
-        'result': flat
+        word: flat
     })
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
     # s = time.time()
-    # print(last('天际'))
+    # print(last('肥美多汁'))
     # e = time.time()
     # print(int(e - s))
